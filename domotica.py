22cf1284af15828c8fda3b8c6683a9070775fc8d
ejmalfatti <ejmalfatti@outlook.com>
2016-07-24 16:55:47 -0300

    Modificacion para funcionar en raspberry pi 3

--- domotica.py
+++ domotica.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8-*-
+import RPi.GPIO as GPIO
+import time
 import os
 import time
 
 i=0
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.OUT) ## GPIO 24 como salida
 
 while i != 1 :
 	infile = open('capture.txt', 'r')
@@ -11,11 +16,13 @@ while i != 1 :
 	for line in infile:
 		if line.find("encender luz") != -1 :
 			print "Encendiendo la luz"
+			GPIO.output(24, False) ## Enciendo el 24
 			#os.system("echo 0 > /sys/class/gpio/gpio17/value")
 			os.system("true > capture.txt")
 			#os.system("festival -b '(SayText "Green led off")'")
 		if line.find("apagar luz") != -1 :
 			print "Apagando la luz"
+			GPIO.output(24, True) ## Apago el 24
 			#os.system("echo 1 > /sys/class/gpio/gpio17/value")
 			os.system("true > capture.txt")
 			#os.system("festival -b '(SayText "Green led ON")'")
