a07b0dbabe101666ece00b83e04cb8ccc8f946e9
ejmalfatti <ejmalfatti@outlook.com>
2016-07-24 16:56:01 -0300

    Modificacion para funcionar en raspberry pi 3

--- pocketsphinx_continuous.py
+++ pocketsphinx_continuous.py
@@ -7,5 +7,5 @@ os.system("rm capture.txt")
 #os.system("./shut.py &")
 
 #os.system("sudo pocketsphinx_continuous -lm 3906.lm -dict 3906.dic > capture.txt -samprate 16000/8000/48000 &")
-os.system("pocketsphinx_continuous -hmm ../voxforge-es-0.2/model_parameters/voxforge_es_sphinx.cd_ptm_3000/ -lm modificados/5298.lm -dict modificados/5298.dic -jsgf modificados/5298.jsgf > capture.txt -samprate 16000/8000/48000 -inmic yes")
+os.system("pocketsphinx_continuous -hmm voxforge-es-0.2/model_parameters/voxforge_es_sphinx.cd_ptm_3000/ -lm diccionario/5298.lm -dict diccionario/5298.dic -jsgf diccionario/5298.jsgf > capture.txt -samprate 16000/8000/48000 -inmic yes -adcdev plughw:0,0")
 os.system("./read.py &")
